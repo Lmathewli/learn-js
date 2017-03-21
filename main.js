d8453d3c126f5bd90c7fd75f534fe10533782b98
Lmathewli <18360904602@163.com>
2017-03-21 17:47:07 +0800

    These files for studying at home.

---
+++ main.js
@@ -0,0 +1,37 @@
+'use strict';
+var greet = require('./hello');
+var files = require('fs');
+
+
+greet('zhangsan');
+
+
+files.readFile('test.txt', 'utf-8', function(error, data) {
+    if (error) {
+        console.log('error');
+    } else {
+        console.log(data);
+    }
+});
+
+files.writeFile('test.txt', 'zhangsan', function(error) {
+    if (error) {
+        console.log('error');
+    } else {
+        console.log('OK');
+    }
+});
+files.stat('test.txt', function(error, stats) {
+     if (error) {
+        console.log('error');
+    } else {
+        console.log(stats.size);
+        // 是否是文件:
+        console.log('isFile: ' + stats.isFile());
+    }
+});
+
+var rs = files.createReadStream('test.txt');
+var ws = files.createWriteStream('copied.txt');
+
+rs.pipe(ws);
